f1f0f923370948ad1044825dcb51ee471993aea6
Mariem <mariem.ehab234@gmail.com>
2024-08-01 12:23:28 +0000

    Remove class"

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,21 +3,6 @@ import logging
 import re
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
-
-
 def filter_datum(fields, redaction, message, separator):
     """Returns the log message obfuscated"""
     for i in fields:
