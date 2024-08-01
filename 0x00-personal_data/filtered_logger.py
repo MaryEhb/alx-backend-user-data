d0c57e7755cb66931c9ec989842fc022ccf5f6c9
Mariem <mariem.ehab234@gmail.com>
2024-08-01 12:27:22 +0000

    file comment

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+'''filtered logger'''
 import logging
 import re
 
@@ -9,3 +10,21 @@ def filter_datum(fields, redaction, message, separator):
         message = re.sub(f'{i}=.*?{separator}',
                          f'{i}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+
+
+
