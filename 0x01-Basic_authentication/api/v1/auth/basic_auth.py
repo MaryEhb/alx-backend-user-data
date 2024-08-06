49473b13bcfc7ed5993af81026c6cb6881d08855
Mariem <mariem.ehab234@gmail.com>
2024-08-06 17:33:38 +0000

    Task 8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''6. Basic auth'''
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -13,3 +14,14 @@ class BasicAuth(Auth):
            or authorization_header[0:6] != 'Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_auth_header: str) -> str:
+        '''returns the decoded value of a Base64 string'''
+        if (not base64_auth_header
+           or not isinstance(base64_auth_header, str)):
+            return None
+        try:
+            return base64.b64decode(base64_auth_header).decode('utf-8')
+        except Exception:
+            return None
