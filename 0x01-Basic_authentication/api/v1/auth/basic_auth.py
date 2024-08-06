34d8a23f1a20271c33e00e7316700ad42076466a
Mariem <mariem.ehab234@gmail.com>
2024-08-06 17:52:10 +0000

    Task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 '''6. Basic auth'''
 from api.v1.auth.auth import Auth
 import base64
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -35,3 +37,20 @@ class BasicAuth(Auth):
             return None, None
         index = decoded_header.index(':')
         return decoded_header[:index], decoded_header[index + 1:]
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        ''' returns the User instance based on his email and password'''
+        if (not user_email or not isinstance(user_email, str)
+           or not user_pwd or not isinstance(user_pwd, str)):
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        if len(users) <= 0:
+            return None
+        if users[0].is_valid_password(user_pwd):
+            return users[0]
+        return None
