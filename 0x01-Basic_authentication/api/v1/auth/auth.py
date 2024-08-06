a23ee3f3410f70f0d0c9fac0674b75f030c4d81a
Mariem <mariem.ehab234@gmail.com>
2024-08-06 16:02:50 +0000

    Task 5"

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,9 @@ class Auth:
     def require_auth(self,
                      path: str,
                      excluded_paths: List[str]) -> bool:
-        '''returns boolan'''
+        '''Define which routes don't need authentication
+        Return: True if the path is not in the list of strings
+        excluded_paths'''
         if not path or not excluded_paths or len(excluded_paths) == 0:
             return True
         if path[-1] != '/':
@@ -18,8 +20,10 @@ class Auth:
         return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
-        '''return none'''
-        return None
+        '''Request validation'''
+        if not request:
+            return None
+        return request.headers.get('Authorization', None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         '''return None'''
