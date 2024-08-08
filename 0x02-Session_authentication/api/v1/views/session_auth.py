147a482942627ce4ee028ed0dd7cc063bef94134
Mariem <mariem.ehab234@gmail.com>
2024-08-08 09:14:32 +0000

    Task 7

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -19,12 +19,17 @@ def login():
         return jsonify({"error": "password missing"}), 400
     try:
         user = User.search(email)
-        user = user[0]
     except Exception:
         return jsonify({"error": "no user found for this email"}), 404
 
     if not user:
         return jsonify({"error": "no user found for this email"}), 404
+
+    for u in user:
+        if not u.is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+
+    user = user[0]
 
     if not user.is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
