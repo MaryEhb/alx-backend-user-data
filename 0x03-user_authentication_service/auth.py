d48b1c96f5b62051773db79c3702fdd7c012e0b8
Mariem <mariem.ehab234@gmail.com>
2024-08-14 11:54:56 +0000

    Task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,3 +27,12 @@ class Auth():
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """check if user cradintials are correct"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except Exception:
+            return False
